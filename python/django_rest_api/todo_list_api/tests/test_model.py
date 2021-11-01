9d37977818cfd3f400d428cb509585be6fec8137
Gabriel-S-Costa <gabriel.silvacosta.gc@gmail.com>
2021-11-01 14:44:47 -0300

    Commit django rest framework

--- python/django_rest_api/todo_list_api/tests/test_model.py
+++ python/django_rest_api/todo_list_api/tests/test_model.py
@@ -9,7 +9,7 @@ class testTaskModel(TestCase):
         print("Set up")
         Task.objects.create(title="Estudar Git", completed=False, created_at='2021-11-01 13:27:00')
         Task.objects.create(title="Estudar Matemática", completed=False, created_at='2021-11-01 13:27:00')
+
     
-    def test_
     # def test_task_detail_list(self):
         
